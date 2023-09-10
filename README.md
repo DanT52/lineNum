851fab95e37bafc4f5752bfe632ee91cbd50a7b3
Daniel Basarab <42653318+DanT52@users.noreply.github.com>
2023-09-10 22:11:08 +0000

    version 9

--- README.md
+++ README.md
@@ -1,12 +1,3 @@
 # notes
 
-- open file
-
-- seek to end to get the dict length
-
-- go to middle and compare with strcmp
-
-- binary search
-
-- not found return -(last index searched) else return line num. 
-which will be current offset / dict width.
+Working version however issue is it matches words unesseserily like cat might match caterpiller...
--- main.c
+++ main.c
@@ -4,21 +4,39 @@
 
 int lineNum(const char *file, const char *word, int num);
 
-int main(int argc, char const *argv[])
-{
-    // Check if the correct number of arguments are provided
-    if (argc != 4)
-    {
-        fprintf(stderr, "Usage: %s word file num\n",argv[0]);
-        return 1;
+// int main(int argc, char const *argv[])
+// {
+//     // Check if the correct number of arguments are provided
+//     if (argc != 4)
+//     {
+//         fprintf(stderr, "Usage: %s word file num\n",argv[0]);
+//         return 1;
+//     }
+
+//     const char *word = argv[1];
+//     const char *file = argv[2];
+//     int num = atoi(argv[3]);  // Convert the string to an integer
+
+//     int result = lineNum(file, word, num);
+//     printf("line num: %d \n", result);
+
+//     return 0;
+// }
+
+
+int main(int argc, char const *argv[]){
+
+    // Define test cases
+    char* words[] = {"a b c", "adul", "readable", "readable"};
+    char* dicts[] = {"tiny_9", "webster_16", "webster_no_read", "asdfsdfsad"};
+    int nums[] = {9, 16, 16, 16};  // Assuming a width of 16 for the last dictionary, adjust as necessary
+    int num_tests = sizeof(words) / sizeof(words[0]);
+
+    for(int i = 0; i < num_tests; i++) {
+        printf("Testing word '%s' with dictionary '%s'...\n", words[i], dicts[i]);
+        int result = lineNum(dicts[i], words[i], nums[i]);
+        printf("Result: line num = %d\n\n", result);
     }
-
-    const char *word = argv[1];
-    const char *file = argv[2];
-    int num = atoi(argv[3]);  // Convert the string to an integer
-
-    int result = lineNum(file, word, num);
-    printf("line num: %d \n", result);
 
     return 0;
 }
